39d8e4b288ead32212154705e459f8033f5ffb9f
Meya <gabzmejia117@gmail.com>
2023-10-29 02:59:33 +0800

    got some inspiration from erussianguy's combat music mod

--- src/main/java/net/meya/battlemusicmod/battlemusic.java
+++ src/main/java/net/meya/battlemusicmod/battlemusic.java
@@ -1,80 +1,89 @@
 package net.meya.battlemusicmod;
 
-import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.resources.sounds.SimpleSoundInstance;
+import net.minecraft.client.sounds.SoundManager;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.Mob;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraft.util.RandomSource;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-import org.slf4j.Logger;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.phys.AABB;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod(battlemusic.MOD_ID)
+
+import static net.meya.battlemusicmod.battlemusic.MOD_ID;
+
+@Mod(MOD_ID)
 public class battlemusic {
     public static final String MOD_ID = "battlemusic";
-    public static final Logger LOGGER = LogUtils.getLogger();
 
-    public class battlesound {
-        public static final DeferredRegister<SoundEvent> SOUND_EVENTS =
-                DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, battlemusic.MOD_ID);
+    public int decaySeconds = 0;
+    public SimpleSoundInstance lastSound;
 
-        public static final RegistryObject<SoundEvent> COMBAT_MUSIC = registerSoundEvents("battle_music");
+    public battlemusic() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
 
-        private static RegistryObject<SoundEvent> registerSoundEvents(String name) {
-            return SOUND_EVENTS.register(name, () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(battlemusic.MOD_ID, name)));
-        }
-
-        public static void register(IEventBus eventBus) {
-            SOUND_EVENTS.register(eventBus);
-            // Register your event handler method for the LivingHurtEvent
-            eventBus.register(net.meya.battlemusicmod.battlemusic.class);
-        }
-
-        // Event handler for LivingHurtEvent
-        @SubscribeEvent
-        public static void onLivingHurt(LivingHurtEvent event) {
-            Entity target = event.getEntity();
-            Entity source = event.getSource().getEntity();
-
-            if (target instanceof Player && source instanceof Mob) {
-                SoundEvent sound = net.meya.battlemusicmod.battlemusic.battlesound.COMBAT_MUSIC.get(); // Fixed this line
-                if (sound != null) {
-                    // Play the sound event when a player is hurt by a hostile mob
-                    ServerPlayer player = (ServerPlayer) target;
-                    player.playSound(sound, 1.0F, 1.0F);
+    @SubscribeEvent
+    public void onClientTick(final TickEvent.ClientTickEvent event) {
+        Minecraft mc = Minecraft.getInstance();
+        if (event.phase == TickEvent.Phase.END && mc.level != null && mc.level.getGameTime() % 20 == 0) {
+            SoundManager manager = mc.getSoundManager();
+            if (manager.isActive(lastSound)) {
+                LocalPlayer player = mc.player;
+                if (player != null && getEntities(player) == 0) {
+                    decaySeconds++;
+                }
+                if (decaySeconds > 20) {
+                    manager.stop(lastSound);
                 }
             }
         }
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event) {
-
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-
+    public void onAttack(final LivingAttackEvent event) {
+        LivingEntity entity = event.getEntity();
+        if (entity instanceof LocalPlayer player) {
+            Minecraft mc = Minecraft.getInstance();
+            SoundManager manager = mc.getSoundManager();
+            if (mc.level != null && mc.level.getDifficulty() != Difficulty.PEACEFUL) {
+                if (getEntities(player) > 0) {
+                    if (!manager.isActive(lastSound)) {
+                        mc.getMusicManager().stopPlaying();
+                        lastSound = pickSound(entity.getRandom());
+                        manager.play(lastSound);
+                    }
+                    decaySeconds = 0;
+                }
+            }
+        }
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
+    private static int getEntities(LocalPlayer player) {
+        return player.clientLevel.getEntitiesOfClass(Monster.class, new AABB(-12D, -10D, -12D, 12D, 10D, 12D).move(player.getX(), player.getY(), player.getZ()), mob -> mob.canAttack(player)).size();
+    }
 
+    private SimpleSoundInstance pickSound(RandomSource rand) {
+        // Replace with your list of sound event names
+        final String[] sounds = {
+                "minecraft:music_disc.pigstep",
+                "minecraft:music_disc.mellohi"
+        };
+
+        final int idx = rand.nextInt(sounds.length);
+        final SoundEvent sound = ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation(sounds[idx]));
+        if (sound == null) {
+            throw new NullPointerException("Invalid sound event resource location detected: " + sounds[idx]);
         }
+        return SimpleSoundInstance.forMusic(sound);
     }
 }
